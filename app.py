7266eac7f04a548e3ae4e3d0f9b2c79538190307
Guillaume Meurillon <guillaume.meurillon@student.howest.be>
2020-04-21 22:10:25 +0200

    Fix: issue with data normalization solved

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ def set_column_name():
 @app.route('/preprocess/columns', methods=['GET', 'POST'])
 def preprocess_columns():
     if is_user_logged_in():
-        if post_has_keys('project', 'columns[]', 'normalisation-method', 'column-output[]'):
+        if post_has_keys('project', 'columns[]', 'normalisation-method'):
             runtime_manager.preprocess_project(project_name=request.form['project'],
                                                method=request.form['normalisation-method'],
                                                columns=request.form.getlist('columns[]'))
